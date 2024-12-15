7d35737021681fc704996b73fe9684989b22c866
Aj <ajayshinde007@gmail.com>
2024-12-15 09:32:19 -0500

    import dog package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ajayshinde007/golang_puppy
 
 go 1.23.3
+
+require github.com/ajayshinde007/golang_dog v0.0.0-20241215142532-9559fc1f9af8 // indirect
--- golang_puppy.go
+++ golang_puppy.go
@@ -1,9 +1,15 @@
 package golang_puppy
 
+import "github.com/ajayshinde007/golang_dog"
+
 func Bark() string {
 	return "woof v1!"
 }
 
 func Barks() string {
 	return "woof! woof! woof! v1"
+}
+
+func BigBark() string {
+	return golang_dog.WhenGrownUp(Barks())
 }
